f7321988ec4508fd44dab888e6ad5e574c546ba7
eldertek <andrelauret@eclipse-technology.eu>
2024-07-02 20:49:20 +0400

    update app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ app.config.from_object('app.config.Config')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'routes.login'
+login_manager.login_view = 'login'
 
 login_manager.login_message = "Veuillez vous connecter pour accéder à cette page."
 login_manager.login_message_category = "info"
