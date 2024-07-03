19686fd9d88232cae20570f0aaf095499a9bbda5
eldertek <andrelauret@eclipse-technology.eu>
2024-07-03 21:12:34 +0400

    update models.py

--- app/models.py
+++ app/models.py
@@ -1,14 +1,12 @@
 from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
-import json
+from app.utils import load_settings
 
 # Load settings to get the admin password
-with open("data/settings.json", "r") as f:
-    settings = json.load(f)
-admin_password = settings.get("admin_password")
+settings = load_settings()
+admin_password = settings.get('admin_password')
 
 # In-memory storage for simplicity
-users = {"admin": {"password": admin_password}}
+users = {'admin': {'password': admin_password}}
 
 class User(UserMixin):
     def __init__(self, id):
@@ -17,12 +15,5 @@ class User(UserMixin):
 def load_user(user_id):
     return User(user_id)
 
-def get_admin_password():
-    return admin_password
-
 def update_admin_password(new_password):
-    from app import db
-    admin_user = User.query.filter_by(username="admin").first()
-    if admin_user:
-        admin_user.password = generate_password_hash(new_password, method='sha256')
-        db.session.commit()
+    users['admin']['password'] = new_password
