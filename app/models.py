c4751dc151304aa6195f6056513093b7f281fde5
eldertek <andrelauret@eclipse-technology.eu>
2024-07-03 21:09:15 +0400

    update models.py and utils.py

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from flask_login import UserMixin
-from app.utils import load_settings
 from werkzeug.security import generate_password_hash
 from app import db
+import json
 
 # Load settings to get the admin password
-settings = load_settings()
+with open("data/settings.json", "r") as f:
+    settings = json.load(f)
 admin_password = settings.get("admin_password")
 
 # In-memory storage for simplicity
