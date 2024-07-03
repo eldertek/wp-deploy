0c2eadae9505253e038603b84e44ec67168185b3
eldertek <andrelauret@eclipse-technology.eu>
2024-07-03 21:28:37 +0400

    update config.py

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ from werkzeug.security import generate_password_hash
 
 class Config:
     SECRET_KEY_FILE = "data/secret_key.json"
-    SETTINGS_FILE = "data/config.json"
+    USERS_FILE = "data/users.json"
 
     if not os.path.exists(SECRET_KEY_FILE):
         secret_key = os.urandom(24).hex()
@@ -17,20 +17,20 @@ class Config:
 
     SECRET_KEY = secret_key
 
-    # Ensure default admin credentials are set in settings.json
-    if not os.path.exists(SETTINGS_FILE):
-        settings = {
+    # Ensure default admin credentials are set in users.json
+    if not os.path.exists(USERS_FILE):
+        users = {
             "admin_username": "admin",
             "admin_password": generate_password_hash("admin")
         }
-        with open(SETTINGS_FILE, "w") as f:
-            json.dump(settings, f, indent=4)
+        with open(USERS_FILE, "w") as f:
+            json.dump(users, f, indent=4)
     else:
-        with open(SETTINGS_FILE, "r") as f:
-            settings = json.load(f)
-            if "admin_username" not in settings:
-                settings["admin_username"] = "admin"
-            if "admin_password" not in settings or not settings["admin_password"].startswith("pbkdf2:sha256:"):
-                settings["admin_password"] = generate_password_hash("admin")
-            with open(SETTINGS_FILE, "w") as f:
-                json.dump(settings, f, indent=4)
+        with open(USERS_FILE, "r") as f:
+            users = json.load(f)
+            if "admin_username" not in users:
+                users["admin_username"] = "admin"
+            if "admin_password" not in users or not users["admin_password"].startswith("pbkdf2:sha256:"):
+                users["admin_password"] = generate_password_hash("admin")
+            with open(USERS_FILE, "w") as f:
+                json.dump(users, f, indent=4)
