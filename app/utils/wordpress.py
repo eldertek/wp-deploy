6a56de199db0157d03065a70f2ac8def2426d598
eldertek <andrelauret@eclipse-technology.eu>
2024-07-08 18:24:38 +0400

    update wordpress.py

--- app/utils/wordpress.py
+++ app/utils/wordpress.py
@@ -167,12 +167,9 @@ def install_wordpress(domain_name, force=False):
         # Restore
         run_command(f"wp ai1wm restore wpocopo.wpress --yes --path={wp_path}")
 
-        # Delete the OCOPO backup
-        run_command(f"wp ai1wm backup delete $(sudo -u www-data wp ai1wm backup list --field=ID --path={wp_path}) --path={wp_path}")
-
         # Delete AIO, hello dolly, akismet
-        #run_command(f"wp plugin delete all-in-one-wp-migration --path={wp_path}")
-        #run_command(f"wp plugin delete aio_unlimited --path={wp_path}")
+        run_command(f"wp plugin delete all-in-one-wp-migration --path={wp_path}")
+        run_command(f"wp plugin delete aio_unlimited --path={wp_path}")
         run_command(f"wp plugin delete akismet --path={wp_path}")
         run_command(f"wp plugin delete hello --path={wp_path}")
 
