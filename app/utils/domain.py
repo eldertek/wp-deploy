af79d151dbf9fb43ef0f62b71798f12ce87475b9
eldertek <andrelauret@eclipse-technology.eu>
2024-08-07 16:33:47 +0400

    update domain.py

--- app/utils/domain.py
+++ app/utils/domain.py
@@ -60,7 +60,7 @@ def configure_dns(domain_name):
 
     # On supprime tous les hosts
     try:
-        socketio.emit("console", "Hotes: " + str(host_client.list_hosts()))
+        socketio.emit("console", "Hotes: " + str(host_client.list_hosts(domain_name)))
     except Exception as e:
         socketio.emit("error", f"Erreur lors de la récupération des hôtes: {str(e)}")
 
