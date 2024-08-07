3d99a76d773d867a8f1e28e234898ff3b3d4cdaa
eldertek <andrelauret@eclipse-technology.eu>
2024-08-07 15:39:27 +0400

    update system.py

--- app/utils/system.py
+++ app/utils/system.py
@@ -1,13 +1,13 @@
 import subprocess
 from app import socketio
 
-def run_command(command, elevated=False, return_output=False, timeout=300):
+def run_command(command, elevated=False, return_output=False):
     if elevated:
         command = f"sudo -s {command}"
     try:
-        # Use subprocess.run with timeout and capture_output for better control
+        # Use subprocess.run without a timeout for long-running commands
         result = subprocess.run(
-            command, shell=True, check=True, capture_output=True, text=True, timeout=timeout
+            command, shell=True, check=True, capture_output=True, text=True
         )
         if return_output:
             return result.stdout  # Return the command output
@@ -17,12 +17,6 @@ def run_command(command, elevated=False, return_output=False, timeout=300):
         # Emit both stdout and stderr in case of error
         socketio.emit("console", f"Erreur: {e}\nSortie: {e.stdout}\nErreur: {e.stderr}")
         return False  # Command failed
-    except subprocess.TimeoutExpired as e:
-        socketio.emit(
-            "error", f"Timeout: La commande a pris trop de temps à s'exécuter"
-        )
-        return False  # Command timed out
     except Exception as e:
         socketio.emit("console", f"Erreur inattendue: {str(e)}")
         return False  # Unexpected error
-    return None
