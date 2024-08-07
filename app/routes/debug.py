2f5ff0e534c7165058b27c98befdfcf9cffb4773
eldertek <andrelauret@eclipse-technology.eu>
2024-08-07 15:32:20 +0400

    update debug.py and debug.html

--- app/routes/debug.py
+++ app/routes/debug.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required
 from app.utils.wordpress import create_nginx_config, setup_ssl
+from app.utils.deployment import deploy_static
 
 debug_bp = Blueprint('debug', __name__)
 
@@ -17,5 +18,8 @@ def debug():
         elif action == "ssl":
             success = setup_ssl(domain)
             return jsonify({"status": "success" if success else "error", "message": "Action traitée par le serveur."})
+        elif action == "deploy":
+            success = deploy_static(domain)  # Appel de la fonction de déploiement
+            return jsonify({"status": "success" if success else "error", "message": "Déploiement forcé."})
         
     return render_template("debug.html")
