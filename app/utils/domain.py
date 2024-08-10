ecad0d38b9a1c938ed63fce05dcaef680b8b8feb
eldertek <andrelauret@eclipse-technology.eu>
2024-08-10 21:33:29 +0400

    update domain.py

--- app/utils/domain.py
+++ app/utils/domain.py
@@ -1,6 +1,7 @@
 from internetbs import Domain, DNS, Host
 from app import socketio
 from .settings import load_settings
+from .system import run_command
 import dns.resolver
 
 settings = load_settings()
@@ -47,74 +48,28 @@ def purchase_domain(domain_name, contacts):
         return None
 
 def configure_dns(domain_name):
-    dns_records = [
-        {"name": f"bo.{domain_name}", "type": "A", "value": "51.210.255.66"},
-        {"name": f"bo.{domain_name}", "type": "AAAA", "value": "2001:41d0:304:200::5ec6"},
-        {"name": domain_name, "type": "A", "value": "51.210.255.66"},
-        {"name": domain_name, "type": "AAAA", "value": "2001:41d0:304:200::5ec6"},
-        {"name": domain_name, "type": "NS", "value": "ns-usa.topdns.com"},
-        {"name": domain_name, "type": "NS", "value": "ns-uk.topdns.com"},
-        {"name": domain_name, "type": "NS", "value": "ns-canada.topdns.com"},
-    ]
+    zone_file_path = f"/etc/bind/db.{domain_name}"
+    temp_zone_file_path = f"/tmp/db.{domain_name}"
 
-    expected_ns = [record["value"] for record in dns_records if record["type"] == "NS"]
+    # Create DNS configuration file
+    with open(temp_zone_file_path, "w") as temp_zone_file:
+        temp_zone_file.write(f"$TTL 86400\n")
+        temp_zone_file.write(f"@   IN  SOA ns1.a1b2c3d4e5.fr. admin.chinavisum24.de. (\n")
+        temp_zone_file.write(f"            2023081001  ; Serial\n")
+        temp_zone_file.write(f"            3600        ; Refresh\n")
+        temp_zone_file.write(f"            1800        ; Retry\n")
+        temp_zone_file.write(f"            1209600     ; Expire\n")
+        temp_zone_file.write(f"            86400 )     ; Negative Cache TTL\n")
+        temp_zone_file.write(f";\n")
+        temp_zone_file.write(f"@       IN  NS  ns1.a1b2c3d4e5.fr.\n")
+        temp_zone_file.write(f"@       IN  NS  ns2.a1b2c3d4e5.fr.\n")
+        temp_zone_file.write(f"@       IN  A   51.210.255.66\n")
+        temp_zone_file.write(f"@       IN  AAAA  2001:41d0:304:200::5ec6\n")
+        temp_zone_file.write(f"bo      IN  A   51.210.255.66\n")
+        temp_zone_file.write(f"bo      IN  AAAA  2001:41d0:304:200::5ec6\n")
 
-    for _ in range(3):
-        for record in dns_records:
-            try:
-                dns_client.remove_record(record["name"], record["type"])
-            except Exception:
-                pass
-
-    try:
-        resolver = dns.resolver.Resolver()
-        resolver.cache = dns.resolver.LRUCache(0)
-        current_ns = [rdata.to_text() for rdata in resolver.resolve(domain_name, 'NS')]
-
-        for ns in expected_ns:
-            api_response, api_url = dns_client.add_record(domain_name, "NS", ns)
-            socketio.emit("console", f"DNS ({api_response.status}) : {domain_name} NS {ns} -> {api_url}")
-
-        if not all(ns in current_ns for ns in expected_ns):
-            socketio.emit("message", f"Les serveurs de noms pour {domain_name} ne sont pas configurés correctement. Configuration en cours...")
-            api_response, api_url = domain_client.update_domain(domain_name, ns_list=expected_ns)
-            socketio.emit("console", f"Configuration NS pour {domain_name} -> {api_url}")
-
-    except (dns.resolver.NXDOMAIN, dns.resolver.NoAnswer, dns.resolver.NoNameservers):
-        socketio.emit("message", f"Les serveurs de noms pour {domain_name} ne sont pas configurés correctement. Configuration en cours...")
-
-        for ns in expected_ns:
-            api_response, api_url = dns_client.add_record(domain_name, "NS", ns)
-            socketio.emit("console", f"DNS ({api_response.status}) : {domain_name} NS {ns} -> {api_url}")
-
-        try:
-            api_response, api_url = domain_client.update_domain(domain_name, ns_list=expected_ns)
-        except Exception as e:
-            socketio.emit("console", f"Erreur lors de la mise à jour du domaine {domain_name}: {str(e)}")
-        socketio.emit("console", f"NXDOMAIN-NoAnswer -> Configuration NS pour {domain_name} -> {api_url}")
-        socketio.emit("message", "La propagation DNS peut prendre plusieurs heures. Veuillez réessayer plus tard.")
-        
-        for ns in expected_ns:
-            api_response, api_url = dns_client.add_record(domain_name, "NS", ns)
-            socketio.emit("console", f"DNS : {domain_name} NS {ns} -> {api_url}")
-        return None
-    except Exception as e:
-        socketio.emit("error", f'Erreur lors de la vérification des serveurs de noms pour {domain_name}. Veuillez réessayer dans quelques minutes.')
-        socketio.emit("console", f"Erreur lors de la vérification des serveurs de noms pour {domain_name}: {type(e).__name__} - {str(e)}")
-        return None
-
-    socketio.emit("message", f"Configuration du DNS pour {domain_name}...")
-
-    try:
-        for record in dns_records:
-            api_response, api_url = dns_client.add_record(record["name"], record["type"], record["value"])
-            socketio.emit("console", f"DNS : {record['name']} {record['type']} {record['value']} -> {api_url}")
-            socketio.emit("console", f"DNS Response: {api_response}")
-            socketio.emit("message", f'Enregistrement DNS {record["type"]} pour {record["name"]} configuré à {record["value"]}.')
-    except Exception as e:
-        socketio.emit("error", f'Erreur lors de la configuration DNS pour {domain_name}: {str(e)}')
-        return None
-    return True
+        # Move the temporary file to the Nginx config directory with elevated privileges
+        run_command(f"mv {temp_zone_file_path} {zone_file_path}", elevated=True)
 
 def check_dns(domain_name):
     expected_records = [
