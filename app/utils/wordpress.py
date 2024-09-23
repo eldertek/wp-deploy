acd32275a685303787ca45381d37a383b8c6f692
eldertek <andrelauret@eclipse-technology.eu>
2024-09-23 17:18:38 +0200

    update wordpress.py

--- app/utils/wordpress.py
+++ app/utils/wordpress.py
@@ -269,9 +269,18 @@ def install_wordpress(domain_name, backup_file_path=None):
 def generate_wp_login_link(domain_name):
     wp_path = f"/var/www/{domain_name}"
     try:
-        command = (
-            f"wp login create admin --path={wp_path} --url=https://bo.{domain_name}"
-        )
+        # Vérifiez si l'utilisateur admin existe
+        check_user_command = f"wp user list --field=user_login --path={wp_path} | grep '^admin$'"
+        user_exists = run_command(check_user_command, return_output=True)
+
+        if not user_exists:
+            # Créer un nouvel utilisateur admin si l'utilisateur n'existe pas
+            new_admin_password = "".join(random.choices(string.ascii_letters + string.digits, k=16))
+            create_user_command = f"wp user create admin {load_settings()['registrant']['email']} --role=administrator --user_pass={new_admin_password} --path={wp_path}"
+            run_command(create_user_command, elevated=True)
+
+        # Créer le lien de connexion
+        command = f"wp login create admin --path={wp_path} --url=https://bo.{domain_name}"
         result = run_command(command, return_output=True)
         if result:
             lines = result.strip().split("\n")
