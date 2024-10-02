d9ae5602cdbd2c9e19a3b3cee7c7ff84776c1ea0
André Théo LAURET <andrelauret@eclipse-technology.eu>
2024-10-02 20:05:07 +0400

    Add success and error message display for individual site deployment
    * Display success message on successful deployment
    * Display error message on failed deployment
    * Remove messages after 5 seconds

--- static/js/index.js
+++ static/js/index.js
@@ -9,9 +9,25 @@ $(document).on('click', '.force-update-btn', function() {
         contentType: 'application/json',
         success: function(response) {
             console.log('Force update initiated for domain:', domain);
+            // Display success message
+            var message = $('<div class="alert alert-success mt-4">Mise à jour réussie pour le domaine: ' + domain + '</div>');
+            $('#socket-messages').append(message);
+            setTimeout(function() {
+                message.fadeOut(1000, function() {
+                    $(this).remove();
+                });
+            }, 5000);
         },
         error: function(error) {
             console.log('Error initiating force update for domain:', domain);
+            // Display error message
+            var message = $('<div class="alert alert-danger mt-4">Erreur lors de la mise à jour pour le domaine: ' + domain + '</div>');
+            $('#socket-messages').append(message);
+            setTimeout(function() {
+                message.fadeOut(1000, function() {
+                    $(this).remove();
+                });
+            }, 5000);
         }
     });
 });
