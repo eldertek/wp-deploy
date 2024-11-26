c48e080576d2ec052bd54d7865eb939a7ed60e4f
eldertek <andrelauret@eclipse-technology.eu>
2024-11-26 18:11:02 +0100

    update gunicorn.py

--- gunicorn.py
+++ gunicorn.py
@@ -2,11 +2,11 @@
 Gunicorn configuration file.
 """
 
-BIND = "127.0.0.1:8000"
-WORKERS = 3
-LIMIT_UPLOAD = 50 * 1024 * 1024 * 1024
-TIMEOUT = 3600
+bind = "127.0.0.1:8000"
+workers = 3
+limit_upload = 50 * 1024 * 1024 * 1024
+timeout = 3600
 
-ERRORLOG = '/var/log/wp-deploy/error.log'
-ACCESSLOG = '/var/log/wp-deploy/access.log'
-LOGLEVEL = 'info'
+errorlog = '/var/log/wp-deploy/error.log'
+accesslog = '/var/log/wp-deploy/access.log'
+loglevel = 'info'
